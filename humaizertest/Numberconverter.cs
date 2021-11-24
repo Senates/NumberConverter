abf1bb529b536417d19ef666216138847d7e2e16
Senates <coopsnoopftw@gmail.com>
2021-11-24 13:15:09 +1300

    Num to English Currency Converter

---
+++ humaizertest/Numberconverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Humanizer;
+
+namespace humaizertest
+{
+    class Numberconverter
+    {
+        static void Main(string[] args)
+        {
+            string text;
+        start:
+            Console.WriteLine("Enter a Number To Convert:");
+            text = Console.ReadLine();
+            if (text.Contains("."))
+            {
+                string[] cents = text.Split('.');
+
+                if (text.Length < 4)
+                {
+                    Console.WriteLine("You must have a number before the decimal place!");
+                    goto start;
+                }
+                    int num = Convert.ToInt32(cents[0]);
+                    int fcent = Convert.ToInt32(cents[1]);
+                if (fcent > 99)
+                {
+                    Console.WriteLine("Too many decimal places!");
+                    goto start;
+                }
+                else
+                {
+                    Console.WriteLine(num.ToWords() + " and " + fcent.ToWords() + " cents ");
+                }
+            }
+            else
+            {
+                int num = Convert.ToInt32(text);
+                Console.Clear();
+                Console.WriteLine(num.ToWords());
+            }
+        }
+    }
+}
---
+++ humaizertest/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using humaizertest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+
+namespace humaizertest
+{
+    class Test
+    {
+        [TestMethod]
+        private void TestMethod1()
+        {
+            string input = "100.50";
+            string[] actual = input.Split('.');
+            string[] expected = { "100", "50" };
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
